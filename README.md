3ff33fc5c9dc633d91a68a12908e6fccc3f0a247
ms <shaheed_manzur@yahoo.com>
2021-04-13 21:15:43 -0400

    Show options selected

--- README.md
+++ README.md
@@ -34,25 +34,6 @@ until finished
 
 End with an example of getting some data out of the system or using it for a little demo
 
-## Running the tests
-
-Explain how to run the automated tests for this system
-
-### Break down into end to end tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
-
-### And coding style tests
-
-Explain what these tests test and why
-
-```
-Give an example
-```
 
 ## Deployment
 
@@ -83,6 +64,7 @@ See also the list of [contributors](https://github.com/your/project/contributors
 This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
 
 ## Acknowledgments
+* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
 
 * Hat tip to anyone whose code was used
 * Inspiration
--- script.js
+++ script.js
@@ -1,14 +1,69 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// different arrays -
+var specialChars = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?',
+  ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+var numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+  'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCaseChars = [];
+  
+// function to fill uppercase array from lower case array
+function fillUpperCaseArray() {
+  for (i=0; i<lowerCaseChars.length; i++) {
+    upperCaseChars.push(lowerCaseChars[i].toUpperCase());
+  }
+}
+// call fillUpperCaseArray to fillup uppercase array
+fillUpperCaseArray();
+
+// console.log(specialChars);
+// console.log(numberChars);
+// console.log(lowerCaseChars);
+// console.log(upperCaseChars);
+
+// variable for password options, with some default values
+var pwdOptions = {
+  pwdLen: 8,
+  specialChars: false,
+  numberChars: false,
+  lowerCaseChars: false,
+  upperCaseChars: false
+};
+// get password option keys in an array
+var keys = Object.keys(pwdOptions);
+
+
+// show selected options at the bottom 
+function showOptions() {
+  optionStr="Chosen Options: Length " + pwdOptions.pwdLen.toString();
+  if (pwdOptions.lowerCaseChars) {
+    optionStr += ", LowerCase";
+  }
+  if (pwdOptions.upperCaseChars) {
+    optionStr += ", UpperCase";
+  }
+  if (pwdOptions.specialChars) {
+    optionStr += ", Special Characters";
+  }
+  if (pwdOptions.numberChars) {
+    optionStr += ", Numbers";
+  }
+  return optionStr;
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var optionText = document.querySelector("#options");
 
-  passwordText.value = password;
-
+  passwordText.value = generatePassword();
+  if (password != "test") {
+    optionText.innerHTML = showOptions();
+  }
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
